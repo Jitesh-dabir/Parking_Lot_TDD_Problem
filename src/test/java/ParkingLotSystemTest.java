e8f06606200e0918a5234cfc4065d61211d23d53
Jitesh-Dabir <jitudabir9@gmail.com>
2020-04-15 17:57:00 +0530

    Added: SetUp method

--- src/test/java/ParkingLotSystemTest.java
+++ src/test/java/ParkingLotSystemTest.java
@@ -1,18 +1,25 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ParkingLotSystemTest {
+    Object vehicle = null;
+    ParkingLotSystem parkingLotSystem = null;
+
+    @Before
+    public void setUp() throws Exception {
+        vehicle = new Object();
+        parkingLotSystem = new ParkingLotSystem();
+    }
+
     @Test
     public void givenAVehicle_WhenParked_ShouldReturnTrue() {
-        ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
         boolean isParked = parkingLotSystem.park(new Object());
         Assert.assertEquals(true, isParked);
     }
 
     @Test
     public void givenAVehicle_WhenUnParked_ReturnTrue() {
-        Object vehicle =new Object();
-        ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
         parkingLotSystem.park(vehicle);
         boolean isUnParked = parkingLotSystem.unPark(vehicle);
         Assert.assertEquals(true, isUnParked);
