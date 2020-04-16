8773c823a53aa132b3e7cde4f8d583392d25c088
Jitesh-Dabir <jitudabir9@gmail.com>
2020-04-16 11:37:07 +0530

    Modified: Test case to check parking lot is full or not using command and query separation principle

--- src/test/java/ParkingLotSystemTest.java
+++ src/test/java/ParkingLotSystemTest.java
@@ -13,22 +13,30 @@ public class ParkingLotSystemTest {
     }
 
     @Test
-    public void givenAVehicle_WhenParked_ShouldReturnTrue() {
-        boolean isParked = parkingLotSystem.park(new Object());
+    public void givenAVehicle_WhenParked_ShouldReturnTrue() throws ParkingLotException {
+        parkingLotSystem.park(vehicle);
+        boolean isParked = parkingLotSystem.isVehicleParked(vehicle);
         Assert.assertEquals(true, isParked);
     }
 
     @Test
-    public void givenAVehicle_WhenAlreadyParked_ShouldReturnFalse() {
-        parkingLotSystem.park(vehicle);
-        boolean isParked = parkingLotSystem.park(new Object());
-        Assert.assertEquals(false, isParked);
+    public void givenAVehicle_WhenAlreadyParked_ShouldReturnFalse() throws ParkingLotException {
+        try {
+            parkingLotSystem.park(vehicle);
+            parkingLotSystem.park(new Object());
+            boolean isParked = parkingLotSystem.isVehicleParked(vehicle);
+            Assert.assertEquals(false, isParked);
+        } catch (ParkingLotException e) {
+            Assert.assertEquals(ParkingLotException.MyException.PARKING_LOT_IS_FULL, e.type);
+        }
+
     }
 
     @Test
-    public void givenAVehicle_WhenUnParked_ReturnTrue() {
+    public void givenAVehicle_WhenUnParked_ReturnTrue() throws ParkingLotException {
         parkingLotSystem.park(vehicle);
-        boolean isUnParked = parkingLotSystem.unPark(vehicle);
+        parkingLotSystem.unPark(vehicle);
+        boolean isUnParked = parkingLotSystem.isVehicleUnParked(vehicle);
         Assert.assertEquals(true, isUnParked);
     }
 }
