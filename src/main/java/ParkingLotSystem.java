cec21b4eb85973ce085bcff27dcb32bd6f03afbb
Jitesh-Dabir <jitudabir9@gmail.com>
2020-04-16 11:42:08 +0530

    Modified: Methods park and unpark car and also use isvehicle park and unpark method to achieve command and query separation principle

--- src/main/java/ParkingLotSystem.java
+++ src/main/java/ParkingLotSystem.java
@@ -5,15 +5,25 @@ public class ParkingLotSystem {
     public ParkingLotSystem() {
     }
 
-    public boolean park(Object vehicle) {
+    public void park(Object vehicle) throws ParkingLotException {
         if (this.vehicle != null)
-            return false;
+            throw new ParkingLotException(ParkingLotException.MyException.PARKING_LOT_IS_FULL, "Parking lot is full");
         this.vehicle = vehicle;
-        return true;
     }
 
-    public boolean unPark(Object vehicle) {
+    public void unPark(Object vehicle){
         if (this.vehicle.equals(vehicle))
+            this.vehicle = null;
+    }
+
+    public boolean isVehicleParked(Object vehicle) {
+        if (this.vehicle.equals(vehicle))
+            return true;
+        return false;
+    }
+
+    public boolean isVehicleUnParked(Object vehicle) {
+        if (this.vehicle != vehicle)
             return true;
         return false;
     }
