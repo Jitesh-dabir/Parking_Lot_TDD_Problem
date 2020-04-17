75e0c2e610d3e3db230808c559b43224fb4a984a
Jitesh-Dabir <jitudabir9@gmail.com>
2020-04-17 16:43:57 +0530

    Added: Method to inform owner when parking lot is full

--- src/main/java/ParkingLotSystem.java
+++ src/main/java/ParkingLotSystem.java
@@ -1,23 +1,36 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class ParkingLotSystem {
 
-    Owner owner = new Owner();
+    private String isFull;
+    private int parkingLotCapacity = 2;
     HashMap<String, Object> parkingLot = new HashMap();
-    boolean isFull, isEmpty;
-
+    private List<IObservable> observableList = new ArrayList<>();
 
     public ParkingLotSystem() {
     }
 
-    public void park(Vehicle vehicle) throws ParkingLotException {
-        if (parkingLot.size() == 2) {
-            owner.setParkingLotStatus("Full");
-            isFull = true;
-            throw new ParkingLotException(ParkingLotException.MyException.PARKING_LOT_IS_FULL, "Parking lot is full");
-        } else {
-            parkingLot.put(vehicle.getVehicleId(), vehicle);
+    public void addObserver(IObservable iObservable) {
+        this.observableList.add(iObservable);
+    }
+
+    public void setInformation(String isFull) {
+        this.isFull = isFull;
+        for (IObservable observable : this.observableList) {
+            observable.update(this.isFull);
         }
+    }
+
+    public void park(Vehicle vehicle) throws ParkingLotException {
+        if (parkingLot.size() < parkingLotCapacity) {
+            parkingLot.put(vehicle.getVehicleId(), vehicle);
+        } else if (parkingLot.size() == parkingLotCapacity) {
+            throw new ParkingLotException(ParkingLotException.MyException.PARKING_LOT_IS_FULL, "Parking lot is full");
+        }
+        if (parkingLot.size() == parkingLotCapacity)
+            setInformation("Full");
     }
 
     public void unPark(Vehicle vehicle) throws ParkingLotException {
@@ -25,8 +38,6 @@ public class ParkingLotSystem {
             throw new ParkingLotException(ParkingLotException.MyException.NO_SUCH_A_VEHICLE, "No such a vehicle");
         else if (parkingLot.containsKey(vehicle.getVehicleId()))
             parkingLot.remove(vehicle.getVehicleId());
-        else if (parkingLot.size() < 2)
-            isEmpty = true;
     }
 
     public boolean isVehicleParked(Vehicle vehicle) {
