5b2ff22aa82a4724d7b7b77dbc73f739c3975da4
Jitesh-Dabir <jitudabir9@gmail.com>
2020-04-17 16:44:52 +0530

    Added: Class for owner

---
+++ src/main/java/Owner.java
@@ -0,0 +1,16 @@
+public class Owner implements IObservable {
+    private String isFull;
+
+    @Override
+    public void update(Object status) {
+        this.setIsFull((String) status);
+    }
+
+    public String getIsFull() {
+        return isFull;
+    }
+
+    public void setIsFull(String isFull) {
+        this.isFull = isFull;
+    }
+}
