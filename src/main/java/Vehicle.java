cb95c6526f339d53b57938b69cb8c470353b3dbf
Jitesh-Dabir <jitudabir9@gmail.com>
2020-04-20 17:11:12 +0530

    Added: Enum for driver type handicap or normal

--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -1,9 +1,19 @@
 public class Vehicle {
+
+    enum DriverType {
+        HANDICAP,NORMAL
+    }
     private String vehicleId;
     private String name;
+    private DriverType driverType;
 
-    public Vehicle(String id, String name) {
+    public Vehicle(String id, String name, DriverType driverType) {
         this.vehicleId = id;
         this.name = name;
+        this.driverType=driverType;
+    }
+
+    public DriverType getDriverType() {
+        return driverType;
     }
 }
