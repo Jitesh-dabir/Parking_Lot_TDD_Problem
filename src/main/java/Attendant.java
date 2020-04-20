ed77d79ebe57cba14662e76145dcb060b51f5596
Jitesh-Dabir <jitudabir9@gmail.com>
2020-04-20 15:18:52 +0530

    Refactored: Method to park and unpark car so cars can evenly distribute to parking lot

--- src/main/java/Attendant.java
+++ src/main/java/Attendant.java
@@ -1,13 +1,37 @@
-import java.util.HashMap;
-import java.util.LinkedHashMap;
+import java.util.*;
 
 public class Attendant {
+    String vehicleKey;
 
-    public void park(HashMap<String, Vehicle> parkingLot, Vehicle vehicle, String getKey) {
-        parkingLot.replace(getKey, vehicle);
+    public void park(HashMap<String, Vehicle> parkingLot, Vehicle vehicle, LinkedHashMap<String, Integer> availableLot) {
+        String keyAvailable = Collections.max(availableLot.entrySet(), Map.Entry.comparingByValue()).getKey();
+        Iterator<String> itr = parkingLot.keySet().iterator();
+        while (itr.hasNext()) {
+            String key = itr.next();
+            if (key.substring(0, 2).equals(keyAvailable) && parkingLot.get(key) == null) {
+                parkingLot.replace(key, vehicle);
+                int value = (availableLot.get(keyAvailable) - 1);
+                availableLot.replace(keyAvailable, value);
+                break;
+            }
+        }
     }
 
-    public void UnPark(LinkedHashMap<String, Vehicle> parkingLot, String getKey) {
-        parkingLot.replace(getKey, null);
+    public void UnPark(LinkedHashMap<String, Vehicle> parkingLot, Vehicle vehicle, LinkedHashMap<String, Integer> availableLot) {
+        Iterator<String> parkingLotItr = parkingLot.keySet().iterator();
+        while (parkingLotItr.hasNext()) {
+            String key = parkingLotItr.next();
+            if (parkingLot.get(key) == vehicle)
+                vehicleKey = key;
+        }
+        parkingLot.replace(vehicleKey, null);
+        Iterator<String> availableLotItr = availableLot.keySet().iterator();
+        while (availableLotItr.hasNext()) {
+            String key = availableLotItr.next();
+            int value = (availableLot.get(key) + 1);
+            if (vehicleKey.substring(0, 2).equals(key)) {
+                availableLot.replace(key, value);
+            }
+        }
     }
 }
