06203c8077e7edc1008970ee4e07b5e7df6bba57
Jitesh-Dabir <jitudabir9@gmail.com>
2020-04-20 19:16:16 +0530

    Modified: Enum for vehicle type large or small in vehicle class

--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -1,16 +1,26 @@
 public class Vehicle {
 
+    enum VehicleType {
+        SMALL, LARGE
+    }
+
     private Driver driver;
     private String vehicleId;
     private String name;
+    private VehicleType vehicleType;
 
-    public Vehicle(String id, String name, Driver driver) {
+    public Vehicle(String id, String name, Driver driver, VehicleType vehicleType) {
         this.vehicleId = id;
         this.name = name;
         this.driver = driver;
+        this.vehicleType = vehicleType;
     }
 
     public Driver getDriver() {
         return driver;
+    }
+
+    public VehicleType getDriverType() {
+        return vehicleType;
     }
 }
