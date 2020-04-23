9642400f01a43e5e493a99804b592559b24abbab
Jitesh-Dabir <jitudabir9@gmail.com>
2020-04-23 18:24:23 +0530

    Refactored: Change values in hashmap for attendant

--- src/main/java/com/bridgelabz/service/ParkingLotSystem.java
+++ src/main/java/com/bridgelabz/service/ParkingLotSystem.java
@@ -4,6 +4,7 @@ import com.bridgelabz.exception.ParkingLotException;
 import com.bridgelabz.observer.IObservable;
 import com.bridgelabz.observer.Owner;
 import com.bridgelabz.utility.ParkingLot;
+import com.bridgelabz.utility.Slot;
 import com.bridgelabz.utility.Vehicle;
 
 import java.util.*;
@@ -11,16 +12,16 @@ import java.util.stream.Collectors;
 
 public class ParkingLotSystem {
 
-    private String isFull;
-    private int parkingLotCapacity = 11;
-    private int numberOfSlot = 4;
-    LinkedHashMap<String, Vehicle> parkingLot = null;
-    LinkedHashMap<String, Integer> availableLot = null;
-    String lotName[] = new String[25];
-    private List<IObservable> observableList = new ArrayList<>();
-    Map<String,Vehicle> listForPoliceDepartment = new HashMap<>();
     Owner owner = null;
+    private String isFull;
+    String lotName[] = new String[25];
     ParkingLot parkingLotHandler = null;
+    Vehicle vehicle = new Vehicle();
+    Slot slot = new Slot(null, null);
+    LinkedHashMap<String, Slot> parkingLot = null;
+    LinkedHashMap<String, Integer> availableLot = null;
+    private List<IObservable> observableList = new ArrayList<>();
+    Map<String, Slot> listForPoliceDepartment = new HashMap<>();
 
     public ParkingLotSystem(Owner owner, ParkingLot parkingLotHandler, LinkedHashMap parkingLot, LinkedHashMap availableLot) {
         this.owner = owner;
@@ -41,41 +42,63 @@ public class ParkingLotSystem {
     }
 
     public void park(Vehicle vehicle) throws ParkingLotException {
-        parkingLotHandler.park(parkingLot, vehicle, availableLot);
-        if (!parkingLot.containsValue(null))
+        if (isParkingLotFull()) {
+            throw new ParkingLotException(ParkingLotException.MyException.PARKING_LOT_IS_FULL, "Parking lot is full");
+        }
+        parkingLotHandler.park(parkingLot, vehicle, availableLot, slot, owner);
+        if (isParkingLotFull())
             setStatus("Full");
     }
 
     public void unPark(Vehicle vehicle) throws ParkingLotException {
-        parkingLotHandler.UnPark(parkingLot, vehicle, availableLot);
-        if (parkingLot.containsValue(null)) {
+        if (vehicle == null) {
+            throw new ParkingLotException(ParkingLotException.MyException.NO_SUCH_A_VEHICLE, "No such a vehicle");
+        }
+        parkingLotHandler.UnPark(parkingLot, vehicle, availableLot, slot);
+        if (!isParkingLotFull()) {
             setStatus("Have Space");
         }
     }
 
     public boolean isVehicleParked(Vehicle vehicle) {
-        if (parkingLot.containsValue(vehicle))
-            return true;
+        Iterator it = parkingLot.keySet().iterator();
+        while (it.hasNext()) {
+            if ((parkingLot.get(it.next()).getVehicle()) == vehicle)
+                return true;
+        }
         return false;
     }
 
     public boolean isVehicleUnParked(Vehicle vehicle) {
-        if (!parkingLot.containsValue(vehicle))
-            return true;
-        return false;
+        Iterator it = parkingLot.keySet().iterator();
+        while (it.hasNext()) {
+            if (((parkingLot.get(it.next()).getVehicle()) == vehicle))
+                return false;
+        }
+        return true;
+    }
+
+    public boolean isParkingLotFull() {
+        Iterator it = parkingLot.keySet().iterator();
+        while (it.hasNext()) {
+            if ((parkingLot.get(it.next()).getVehicle()) == null)
+                return false;
+        }
+        return true;
     }
 
     public String getMyCarParkingNumber(Vehicle vehicle) {
         Iterator<String> itr = parkingLot.keySet().iterator();
         while (itr.hasNext()) {
             String key = itr.next();
-            if (parkingLot.get(key) == vehicle)
+            if (vehicle.equals(parkingLot.get(key).getVehicle()))
                 return key;
         }
         return null;
     }
 
-    public void createParkingLot() {
+    public void createParkingLot(int parkingLotCapacity, int numberOfSlot) {
+        owner.assignAttendantForParkingLot(numberOfSlot);
         int counter = 1, index = 0, slot = 1, length = 0, slotCapacity = 0;
         String letters = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z";
         lotName = letters.split(" ");
@@ -95,7 +118,7 @@ public class ParkingLotSystem {
                 number1 = "0" + number1;
             }
             String key = lotName[slot - 1] + number1;
-            parkingLot.put(key, null);
+            parkingLot.put(key, this.slot);
             index++;
             counter++;
             if (slot == numberOfSlot)
@@ -103,8 +126,13 @@ public class ParkingLotSystem {
         }
     }
 
-    public Map<String, Vehicle> getRecordsByVehicleColorForPolice(Vehicle.Color color) {
-        return listForPoliceDepartment = parkingLot.entrySet().stream().filter(entry -> color.equals(entry.getValue().getColor()))
+    public Map<String, Slot> getRecordsByVehicleColorForPolice(Vehicle.Color color) {
+        return parkingLot.entrySet().stream().filter(entry -> color.equals(entry.getValue().getVehicle().getColor()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    public Map<String, Slot> getRecordsByVehicleColorAndName(Vehicle.Vehicle_Brand brand, Vehicle.Color color) {
+        return parkingLot.entrySet().stream().filter(entry -> color.equals(entry.getValue().getVehicle().getColor()) && brand.equals(entry.getValue().getVehicle().getVehicle_brand()))
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 }
--- src/main/java/com/bridgelabz/utility/ParkingLot.java
+++ src/main/java/com/bridgelabz/utility/ParkingLot.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.utility;
 
 import com.bridgelabz.exception.ParkingLotException;
+import com.bridgelabz.observer.Owner;
 import com.bridgelabz.utility.Driver;
 
 import java.util.*;
@@ -14,34 +15,31 @@ public class ParkingLot {
     String unParkMethodForLargeVehicle = "UN_PARK_LARGE_VEHICLE";
     int value;
 
-    public void park(HashMap<String, Vehicle> parkingLot, Vehicle vehicle, LinkedHashMap<String, Integer> availableLot) throws ParkingLotException {
-        if (!parkingLot.containsValue(null)) {
-            throw new ParkingLotException(ParkingLotException.MyException.PARKING_LOT_IS_FULL, "Parking lot is full");
-        }
+    public void park(HashMap<String, Slot> parkingLot, Vehicle vehicle, LinkedHashMap<String, Integer> availableLot, Slot slot, Owner owner) throws ParkingLotException {
         String keyAvailable = Collections.max(availableLot.entrySet(), Map.Entry.comparingByValue()).getKey();
         Iterator<String> parkingLotItr = parkingLot.keySet().iterator();
         while (parkingLotItr.hasNext()) {
             vehicleKey = parkingLotItr.next();
-            if (vehicle.getDriver().getDriverType().equals(Driver.DriverType.HANDICAP) && parkingLot.get(vehicleKey) == null) {
-                parkingLot.replace(vehicleKey, vehicle);
+            if (vehicle.getDriver().getDriverType().equals(Driver.DriverType.HANDICAP) && parkingLot.get(vehicleKey).getVehicle() == null) {
+                parkingLot.replace(vehicleKey, new Slot(vehicle, owner.getAttendant()));
                 updateAvailableLot(availableLot, vehicleKey, parkMethod);
                 break;
-            } else if (vehicleKey.substring(0, 1).equals(keyAvailable) && parkingLot.get(vehicleKey) == null && !vehicle.getVehicleType().equals(Vehicle.VehicleType.LARGE)) {
-                parkingLot.replace(vehicleKey, vehicle);
+            } else if (vehicleKey.substring(0, 1).equals(keyAvailable) && parkingLot.get(vehicleKey).getVehicle() == null && !vehicle.getVehicleType().equals(Vehicle.VehicleType.LARGE)) {
+                parkingLot.replace(vehicleKey, new Slot(vehicle, owner.getAttendant()));
                 updateAvailableLot(availableLot, vehicleKey, parkMethod);
                 break;
             }
             String nextVehicleKey = getNextKey(vehicleKey, parkingLot);
-            if (vehicle.getVehicleType().equals(Vehicle.VehicleType.LARGE) && parkingLot.get(vehicleKey) == null && parkingLot.get(vehicleKey) == parkingLot.get(nextVehicleKey)) {
-                parkingLot.replace(vehicleKey, vehicle);
-                parkingLot.replace(nextVehicleKey, vehicle);
+            if (vehicle.getVehicleType().equals(Vehicle.VehicleType.LARGE) && parkingLot.get(vehicleKey).getVehicle() == null && parkingLot.get(vehicleKey).getVehicle() == parkingLot.get(nextVehicleKey).getVehicle()) {
+                parkingLot.replace(vehicleKey, new Slot(vehicle, owner.getAttendant()));
+                parkingLot.replace(nextVehicleKey, new Slot(vehicle, owner.getAttendant()));
                 updateAvailableLot(availableLot, vehicleKey, parkMethodForLargeVehicle);
                 break;
             }
         }
     }
 
-    public String getNextKey(String vehicleKey, HashMap<String, Vehicle> parkingLot) throws ParkingLotException {
+    public String getNextKey(String vehicleKey, HashMap<String, Slot> parkingLot) throws ParkingLotException {
         try {
             Iterator<String> parkingLotItr = parkingLot.keySet().iterator();
             while (parkingLotItr.hasNext()) {
@@ -78,31 +76,28 @@ public class ParkingLot {
         }
     }
 
-    public void UnPark(LinkedHashMap<String, Vehicle> parkingLot, Vehicle vehicle, LinkedHashMap<String, Integer> availableLot) throws ParkingLotException {
-        if (vehicle == null) {
-            throw new ParkingLotException(ParkingLotException.MyException.NO_SUCH_A_VEHICLE, "No such a vehicle");
-        }
-        Iterator<String> parkingLotItr = parkingLot.keySet().iterator();
+    public void UnPark(LinkedHashMap<String, Slot> parkingLot1, Vehicle vehicle, LinkedHashMap<String, Integer> availableLot, Slot slot) throws ParkingLotException {
+        Iterator<String> parkingLotItr = parkingLot1.keySet().iterator();
         while (parkingLotItr.hasNext()) {
             String key = parkingLotItr.next();
-            if (parkingLot.get(key) == vehicle) {
+            if (parkingLot1.get(key).getVehicle().equals(vehicle)) {
                 vehicleKey = key;
                 break;
             }
         }
-        if (vehicle.getVehicleType().equals(Vehicle.VehicleType.LARGE) && !vehicleKey.equals(getLastKey(parkingLot))) {
-            nextVehicleKey = getNextKey(vehicleKey, parkingLot);
-            parkingLot.replace(vehicleKey, null);
-            parkingLot.replace(nextVehicleKey, null);
+        if (vehicle.getVehicleType().equals(Vehicle.VehicleType.LARGE) && !vehicleKey.equals(getLastKey(parkingLot1))) {
+            nextVehicleKey = getNextKey(vehicleKey, parkingLot1);
+            parkingLot1.replace(vehicleKey, slot.resetSlot());
+            parkingLot1.replace(nextVehicleKey, slot.resetSlot());
             updateAvailableLot(availableLot, vehicleKey, unParkMethodForLargeVehicle);
         } else {
-            parkingLot.replace(vehicleKey, null);
+            parkingLot1.replace(vehicleKey, slot.resetSlot());
             updateAvailableLot(availableLot, vehicleKey, unParkMethod);
         }
     }
 
-    private String getLastKey(LinkedHashMap<String, Vehicle> parkingLot) {
-        for (Map.Entry<String, Vehicle> entry : parkingLot.entrySet()) {
+    private String getLastKey(LinkedHashMap<String, Slot> parkingLot) {
+        for (Map.Entry<String, Slot> entry : parkingLot.entrySet()) {
             String lastKey = entry.getKey();
             return lastKey;
         }
