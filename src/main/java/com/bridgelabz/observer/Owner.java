d04454499a4f3d195a19e876b6683da38b937c48
Jitesh-Dabir <jitudabir9@gmail.com>
2020-04-23 18:26:46 +0530

    Added: Methods to assign and get attendants for parking lot

--- src/main/java/com/bridgelabz/observer/Owner.java
+++ src/main/java/com/bridgelabz/observer/Owner.java
@@ -1,7 +1,17 @@
 package com.bridgelabz.observer;
 
+import com.bridgelabz.utility.Attendant;
+
+import java.util.List;
+
 public class Owner implements IObservable {
+    private List<Attendant> attendants;
     private String parkingLotStatus;
+    private int numberOfSlot;
+
+    public Owner(List<Attendant> attendants) {
+        this.attendants = attendants;
+    }
 
     @Override
     public void update(Object status) {
@@ -14,5 +24,16 @@ public class Owner implements IObservable {
 
     public void setParkingLotStatus(String isFull) {
         this.parkingLotStatus = isFull;
+    }
+
+    public void assignAttendantForParkingLot(int numberOfSlot) {
+        this.numberOfSlot = numberOfSlot;
+        for (int i = 0; i < numberOfSlot; i++) {
+            attendants.add(new Attendant("Attendant" + Integer.toString(i + 1)));
+        }
+    }
+
+    public Attendant getAttendant() {
+        return attendants.get((int) (Math.random() * numberOfSlot));
     }
 }
