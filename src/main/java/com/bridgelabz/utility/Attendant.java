d881c31d4e079734c71e67de72cb5cef8ad8d25d
Jitesh-Dabir <jitudabir9@gmail.com>
2020-04-23 18:28:35 +0530

    Added: Class for attendant

---
+++ src/main/java/com/bridgelabz/utility/Attendant.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.utility;
+
+public class Attendant {
+    String name;
+
+    public Attendant(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
